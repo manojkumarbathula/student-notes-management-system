9f59aecea2ec72161d93b195b278337d49edc0f3
BATHULA MANOJ KUMAR <bathulamanojkumar11@gmail.com>
2026-09-24 20:40:36 +0530

    Secure email configuration

--- cmail.py
+++ cmail.py
@@ -1,14 +1,23 @@
 import smtplib
+import os
 from email.message import EmailMessage
-app_password='ykqa fzbh hvjy htaw'
-def send_mail(to,body,subject):
-  server=smtplib.SMTP_SSL('smtp.gmail.com',465)
-  server.login('bathulamanojkumar11@gmail.com',app_password)
-  msg=EmailMessage()
-  msg['FROM']='bathulamanojkumar11@gmail.com'
-  msg['TO']=to
-  msg['SUBJECT']=subject
-  msg.set_content(body)
-  server.send_message(msg)
-  server.close()
-  
+from dotenv import load_dotenv
+
+load_dotenv()
+
+def send_mail(to, body, subject):
+    server = smtplib.SMTP_SSL("smtp.gmail.com", 465)
+
+    server.login(
+        os.environ.get("EMAIL_USER"),
+        os.environ.get("EMAIL_PASSWORD")
+    )
+
+    msg = EmailMessage()
+    msg["From"] = os.environ.get("EMAIL_USER")
+    msg["To"] = to
+    msg["Subject"] = subject
+    msg.set_content(body)
+
+    server.send_message(msg)
+    server.close()
